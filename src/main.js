bb2a1c97a539185a163196469bc50342f76326b9
Ivanov Oleksandr <ivanov.oleksandr@intenselab.com>
2019-01-10 17:11:46 +0200

    Directives

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('highlight', {
+    bind(el, binding, vnode) {
+        // el.style.backgroundColor = 'green';
+        // el.style.backgroundColor = binding.value;
+        let delay = 0;
+        if (binding.modifiers['delayed']) {
+            delay = 3000;
+        }
+        setTimeout(() => {
+            if (binding.arg === 'background') {
+                // binding.arg = v-highlight:background
+                el.style.backgroundColor = binding.value
+            } else {
+                el.style.color = binding.value;
+            }
+        }, delay)
+
+    }
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
